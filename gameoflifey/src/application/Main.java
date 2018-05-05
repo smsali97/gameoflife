e4623e0a363e01ac82f2bd2baf6c042c0ad69499
AC <AC@169.254.128.160>
2018-05-06 02:50:10 +0500

    Added the interactive GUI Elements

--- gameoflifey/src/application/Main.java
+++ gameoflifey/src/application/Main.java
@@ -2,112 +2,129 @@ package application;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	Rectangle[][] matrix;
-	private static final int DIM = 1000;
-	private int N = 100;
+	private static final int DIM = 600;
+	private int N = 60;
+
+	private Color active = Color.LIGHTSKYBLUE;
+	private Color inactive = Color.gray(0.2);
+	private Color changed = Color.YELLOW; 
+
+	private final long time = 400_000_000;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			HBox root = new HBox();
+			root.setAlignment(Pos.CENTER);
 			Grid grid = new Grid(N);
 
 			Canvas canvas = new Canvas(DIM, DIM);
-			root.getChildren().add(canvas);
+			VBox panel = createPanel();
+			root.getChildren().addAll(panel, canvas);
 
 			GraphicsContext gc = canvas.getGraphicsContext2D();
 			gc.setFill(Color.BLACK);
 			gc.rect(0, 0, DIM, DIM);
 
 			// Spawning
-//			grid.spawnExplorer(N / 2, N / 2);
 			grid.spawnExplorer(N / 4, N / 4);
-			grid.spawnBeeHive(N/8, N/8);
-			grid.spawnBlinker(N/2, N/2);
-//			grid.spawnBlinker(N/4, N/4);
-			
+			grid.spawnBeeHive(N / 8, N / 8);
+			grid.spawnBlinker(N / 2, N / 2);
+			// grid.spawnBlinker(N/4, N/4);
+
 			consolePrint(grid.grid);
-			
-			int width = DIM/N;
-			
+
+			int width = DIM / N;
+
 			gc.setFill(Color.BLACK);
 			gc.fillRect(0, 0, DIM, DIM);
-			
+
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
 
 					if (grid.grid[i][j].isPopulated()) {
-						gc.setFill(Color.BLUE);
-						//gc.fillRect(i * width, j * width, width, width);
+						gc.setFill(active);
 						gc.fillText("■", i * width, j * width, width);
-						
+
 					} else {
-						gc.setFill(Color.RED);
-						//gc.fillRect(i * width, j * width, width, width);
+						gc.setFill(inactive);
 						gc.fillText("■", i * width, j * width, width);
 					}
 				}
-			
+
 			}
-			
-			
 
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
+
 			AnimationTimer timer = new AnimationTimer() {
 				long lastUpdate = 0;
+
 				@Override
 				public void handle(long now) {
-					if (now - lastUpdate > 1_000_000_000) {
-						lastUpdate = now ;
-						
-						
+					if (now - lastUpdate > time) {
+						lastUpdate = now;
+
 						gc.setFill(Color.BLACK);
 						gc.fillRect(0, 0, DIM, DIM);
-						int ctr = 0;
-						
-						
-						for (int i = 1; i < N-1; i++) {
-							for (int j = 1; j < N-1; j++) {
+
+						for (int i = 1; i < N - 1; i++) {
+							for (int j = 1; j < N - 1; j++) {
 								if (grid.grid[i][j].isPopulated()) {
-									gc.setFill(Color.BLUE);
-									//gc.fillRect(i * width, j * width, width, width);
+									gc.setFill(grid.grid[i][j].active ? changed : active);
+									// gc.fillRect(i * width, j * width, width, width);
 									gc.fillText("■", i * width, j * width, width);
-									
+
 								} else {
-									gc.setFill(Color.DARKRED);
-									//gc.fillRect(i * width, j * width, width, width);
+									gc.setFill(inactive);
+									// gc.fillRect(i * width, j * width, width, width);
 									gc.fillText("■", i * width, j * width, width);
 								}
 							}
 						}
 						grid.checkRules();
-						
+
 					}
 				}
 			};
 
-			timer.start(); 
-			
+			timer.start();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public static void consolePrint(Cell[][] grid) {
 		int N = grid.length;
 		for (int i = 0; i < N; i++) {
@@ -119,17 +136,65 @@ public class Main extends Application {
 		}
 	}
 
-	public static Rectangle[][] computeGrid(int N) {
-		Rectangle[][] grid = new Rectangle[N][N];
+	public static VBox createPanel() {
 
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid.length; j++) {
-				grid[i][j] = new Rectangle(DIM, DIM);
-				grid[i][j].setFill((Color.BLACK));
+		ComboBox<String> box = new ComboBox<>();
+		box.getItems().addAll("Beehive", "Blinker", "Toad", "Glider", "Pentadecathlon", "Pulsar", "Exploder");
+		box.setValue("Glider");
+
+		TextField rowText = new TextField();
+		rowText.setPromptText("Enter row number");
+		TextField colText = new TextField();
+		colText.setPromptText("Enter column number");
+
+		CheckBox cb = new CheckBox("Random");
+		cb.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> ov, Boolean prev, Boolean curr) {
+				if (curr) {
+					rowText.setText("-1");
+					rowText.setEditable(false);
+					colText.setText("-1");
+					colText.setEditable(false);
+				} else {
+					rowText.setText("");
+					rowText.setPromptText("Enter row number");
+					rowText.setEditable(true);
+					colText.setText("");
+					colText.setPromptText("Enter column number");
+					colText.setEditable(true);
+				}
+				// TODO Auto-generated method stub
+
 			}
-		}
+		});
 
-		return grid;
+		Button button = new Button("SPAWN!");
+		button.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		VBox panel = new VBox(10);
+		panel.setAlignment(Pos.CENTER);
+
+		panel.getChildren().addAll(getFancyLabel(), box, cb, new HBox(rowText, colText), button);
+
+		return panel;
+	}
+	
+	public static Label getFancyLabel() {
+		Text text = new Text();
+		Label l = new Label();
+		l.setFont(new Font(30));
+		l.setText("The Game of Life");
+		
+		return l;
+		
 	}
 
 	public static void main(String[] args) {
