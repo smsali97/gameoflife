0688af7f20564d9c3878908a77fbb2fc46aa4873
AC <AC@169.254.128.160>
2018-05-05 16:44:39 +0500

    Made it functional! ^_^

--- gameoflifey/src/application/Grid.java
+++ gameoflifey/src/application/Grid.java
@@ -3,9 +3,9 @@ package application;
 public class Grid {
 	public Cell[][] grid;
 
-	public Grid(int N ) {
+	public Grid(int N) {
 		grid = new Cell[N][N];
-		
+
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid.length; j++) {
 				grid[i][j] = new Cell();
@@ -13,73 +13,126 @@ public class Grid {
 		}
 	}
 
-	public void spawnBeeHive(int r, int c) {
-		if ( r  + 3>= grid.length || c + 3  >= grid.length) return;
-		
-		grid[r][c+1].populate();
-		grid[r][c+2].populate();
-		grid[r+1][c].populate();
-		grid[r+2][c].populate();
-		grid[r+3][c+1].populate();
-		grid[r+3][c+2].populate();
-		
-		grid[r+1][c+2].die();
-		grid[r+1][c+1].die();
+	public void spawnBlinker(int r, int c) {
+		grid[r - 1][c].populate();
+		grid[r + 1][c].populate();
+		grid[r][c].populate();
 	}
-	
-	
-	public int getCellCount(int r, int c) {
-		int ctr = 0;
-		for (int i = r -1; i <= r + 1; i++) {
-			if (i == -1 || i == grid.length) continue;
 
-			for (int j = c -1; j <= c + 1; j++) {
-				if ( j == c || j == -1 || j == grid.length || (i== r && j == c)) continue;
-				if (grid[i][j].isPopulated()) ctr++;
+	public void spawnBeeHive(int r, int c) {
+		if (r + 3 >= grid.length || c + 3 >= grid.length)
+			return;
+
+		grid[r][c + 1].populate();
+		grid[r][c + 2].populate();
+		
+		grid[r + 1][c].populate();
+		grid[r + 1][c+3].populate();
+		
+		grid[r + 2][c + 1].populate();
+		grid[r + 2][c + 2].populate();
+		
+		grid[r + 1][c + 2].die();
+		grid[r + 1][c + 1].die();
+	}
+
+	public int getCellCount(int r, int c) {
+		/*
+		 * int ctr = 0; for (int i = r -1; i <= r + 1; i++) { if (i == -1 || i ==
+		 * grid.length) continue;
+		 * 
+		 * for (int j = c -1; j <= c + 1; j++) { if ( j == grid.length || j == -1 ||
+		 * (i== r && j == c)) continue; if (grid[i][j].isPopulated()) ctr++; } } return
+		 * ctr;
+		 */
+
+		int l = r;
+		int m = c;
+		int aliveNeighbours = 0;
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (grid[r + i][m + j].isPopulated())
+					aliveNeighbours++;
 			}
 		}
-		return ctr;
+		// The cell needs to be subtracted from
+		// its neighbours as it was counted before
+		if (aliveNeighbours != 0)
+			System.out.println(aliveNeighbours);
+		return aliveNeighbours;
+
 	}
-	
+
 	public void spawnExplorer(int r, int c) {
-		if ( r  + 3>= grid.length || c + 3  >= grid.length) return;
-		
-		grid[r-1][c].populate();
-		grid[r][c+1].populate();
-		grid[r+1][c].populate();
-		grid[r+1][c-1].populate();
-		grid[r+1][c+1].populate();
+		if (r + 3 >= grid.length || c + 3 >= grid.length)
+			return;
+
+		grid[r - 1][c].populate();
+		grid[r][c + 1].populate();
+		grid[r + 1][c].populate();
+		grid[r + 1][c - 1].populate();
+		grid[r + 1][c + 1].populate();
 
 	}
 
+	public void checkRules() {
 
-
-	public void checkRules(int r, int c) {
-		int neighbors = this.getCellCount(r,c);
-
-		// For a space that is 'populated':
-		if (grid[r][c].isPopulated()) {
-
-			 //    Each cell with one or no neighbors dies, as if by solitude. 
-			 if (neighbors <= 1) grid[r][c].die();
-			 //    Each cell with four or more neighbors dies, as if by overpopulation. 
-			 else if (neighbors >= 1) grid[r][c].die();
-			 //    Each cell with two or three neighbors survives.
-			 else  grid[r][c].populate(); 
+		Cell[][] new_grid = new Cell[grid.length][grid.length];
+		// copy old grid to new grid
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid.length; j++) {
+				new_grid[i][j] = new Cell();
+				if (grid[i][j].isPopulated())
+					new_grid[i][j].populate();
+			}
 		}
 
-		// For a space that is 'empty' or 'unpopulated'
-		else {
-			//   Each cell with three neighbors becomes populated.
-			if (neighbors == 3) grid[r][c].populate();
+		for (int r = 1; r < grid.length -1; r++) {
+			for (int c = 1; c < grid.length - 1; c++) {
+				int neighbors = 0;
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; j++) {
+						if (grid[r + i][c + j].isPopulated())
+							neighbors++;
+					}
+				}
+				if (grid[r][c].isPopulated()) neighbors--;
+
+				// For a space that is 'populated':
+				if (grid[r][c].isPopulated()) {
+
+					// Any live cell with fewer than two live neighbors dies, as if caused by under
+					// population.
+					if (neighbors < 2)
+						new_grid[r][c].die();
+					// Any live cell with two or three live neighbors lives on to the next
+					// generation.
+					else if (neighbors <= 3)
+						new_grid[r][c].populate();
+					// Any live cell with more than three live neighbors dies, as if by
+					// overpopulation.
+					else
+						new_grid[r][c].die();
+
+				}
+
+				// For a space that is 'empty' or 'unpopulated'
+				else {
+					// Any dead cell with exactly three live neighbors becomes a live cell, as if by
+					// reproduction.
+					if (neighbors == 3)
+						new_grid[r][c].populate();
+				}
+			}
 		}
-	 
+
+		grid = new_grid;
 	}
 }
 
 class Cell {
 	private boolean populated = false;
-
+	
 	public Cell() {
 		populated = false;
 	}
@@ -92,5 +145,7 @@ class Cell {
 		populated = false;
 	}
 
-	public boolean isPopulated() { return populated; }
+	public boolean isPopulated() {
+		return populated;
+	}
 }
--- gameoflifey/src/application/Main.java
+++ gameoflifey/src/application/Main.java
@@ -5,8 +5,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -15,8 +13,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	Rectangle[][] matrix;
-	private static final int DIM = 250;
-	private int N = 20;
+	private static final int DIM = 1000;
+	private int N = 100;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -32,15 +30,37 @@ public class Main extends Application {
 			gc.rect(0, 0, DIM, DIM);
 
 			// Spawning
-			grid.spawnExplorer(N / 2, N / 2);
+//			grid.spawnExplorer(N / 2, N / 2);
 			grid.spawnExplorer(N / 4, N / 4);
+			grid.spawnBeeHive(N/8, N/8);
+			grid.spawnBlinker(N/2, N/2);
+//			grid.spawnBlinker(N/4, N/4);
+			
+			consolePrint(grid.grid);
+			
+			int width = DIM/N;
+			
+			gc.setFill(Color.BLACK);
+			gc.fillRect(0, 0, DIM, DIM);
+			
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					int x = grid.grid[i][j].isPopulated() ? 1 : 0;
-					System.out.print(x + " ");
+
+					if (grid.grid[i][j].isPopulated()) {
+						gc.setFill(Color.BLUE);
+						//gc.fillRect(i * width, j * width, width, width);
+						gc.fillText("■", i * width, j * width, width);
+						
+					} else {
+						gc.setFill(Color.RED);
+						//gc.fillRect(i * width, j * width, width, width);
+						gc.fillText("■", i * width, j * width, width);
+					}
 				}
-				System.out.println();
+			
 			}
+			
+			
 
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -51,35 +71,51 @@ public class Main extends Application {
 				long lastUpdate = 0;
 				@Override
 				public void handle(long now) {
-					if (now - lastUpdate > 2_000_000_000) {
+					if (now - lastUpdate > 1_000_000_000) {
 						lastUpdate = now ;
 						
-						int val = DIM / (N);
+						
 						gc.setFill(Color.BLACK);
 						gc.fillRect(0, 0, DIM, DIM);
 						int ctr = 0;
-
-						for (int i = 0; i < N; i++) {
-							for (int j = 0; j < N; j++) {
-
-								grid.checkRules(i, j);
+						
+						
+						for (int i = 1; i < N-1; i++) {
+							for (int j = 1; j < N-1; j++) {
 								if (grid.grid[i][j].isPopulated()) {
 									gc.setFill(Color.BLUE);
-									gc.fillText("1", i * val, j * val, 4);
+									//gc.fillRect(i * width, j * width, width, width);
+									gc.fillText("■", i * width, j * width, width);
+									
 								} else {
-									gc.setFill(Color.RED);
-									gc.fillText("0", i * val, j * val, 4);
+									gc.setFill(Color.DARKRED);
+									//gc.fillRect(i * width, j * width, width, width);
+									gc.fillText("■", i * width, j * width, width);
 								}
 							}
 						}
+						grid.checkRules();
+						
 					}
 				}
 			};
 
-			timer.start();
-
+			timer.start(); 
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void consolePrint(Cell[][] grid) {
+		int N = grid.length;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				int x = grid[i][j].isPopulated() ? 1 : 0;
+				System.out.print(x + " ");
+			}
+			System.out.println();
 		}
 	}
 
